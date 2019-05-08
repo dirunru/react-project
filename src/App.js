ed94df88e88c1a975e3cd8d9d38dd36d2fa26624
dirunru <2315198050@qq.com>
2019-05-08 13:08:29 +0800

    app的两种渲染方法

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+// 两种渲染方法
+// import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello react</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <h1>hello react</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React,{Component} from 'react';
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>hello react</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
