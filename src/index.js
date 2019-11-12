73ca0bdce73b9e01c485ff9926d15211841c26ad
底润茹 <dirunru@dirunrudeMacBook-Pro.local>
2019-11-12 16:13:03 +0800

    tijiao

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+// react 的入口文件 index.js ，渲染app
+
+//引入入第三方模块不许要加点
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';  //渲染必须要引入react-dom
+
+
+//引入自定义的模块需要加点
+import './index.css'; //css样式
+import App from './App';//渲染app模块，需要引入app
 import * as serviceWorker from './serviceWorker';
 
+//将app组件渲染到index组件的div上
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
